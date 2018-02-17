51fb3dcb9aba8de3ffa9ff2037dd788f28f2ad41
Andrew Wang <azwang@caltech.edu>
2018-02-16 22:54:12 -0800

    Do TA fix for sim.py bug

--- sim.py
+++ sim.py
@@ -84,7 +84,8 @@ def run_simulation(adj_list, node_mappings):
   # converge.
   prev = None
   nodes = adj_list.keys()
-  while not is_stable(generation, randint(100, 200), prev, node_color):
+  last_iter = randint(100, 200)
+  while not is_stable(generation, last_iter, prev, node_color):
     prev = deepcopy(node_color)
     for node in nodes:
       (changed, color) = update(adj_list, prev, node)
@@ -123,13 +124,13 @@ def update(adj_list, node_color, node):
   Updates each node based on its neighbors.
   """
   neighbors = adj_list[node]
-  colored_neighbors = list(filter(None, [node_color[x] for x in neighbors]))
+  colored_neighbors = filter(None, [node_color[x] for x in neighbors])
   team_count = Counter(colored_neighbors)
   if node_color[node] is not None:
     team_count[node_color[node]] += 1.5
   most_common = team_count.most_common(1)
   if len(most_common) > 0 and \
-    most_common[0][1] > len(colored_neighbors) / 2.0:
+    most_common[0][1] > (len(colored_neighbors) + (1.5 if node_color[node] is not None else 0)) / 2.0:
     return (True, most_common[0][0])
 
   return (False, node_color[node])
@@ -167,4 +168,4 @@ def get_result(colors, node_color):
 
 
 if __name__ == '__main__':
-  print (USAGE)
+  print USAGE
